7d33f8a91bc00f746750288ce73ad32489d00c4c
Kenneth Quan <KEQU2@hscic.gov.uk>
2023-12-29 16:52:45 +0000

    getting ref number

--- All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
+++ All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
@@ -1,4 +1,7 @@
 from src.fca_web_scrape import fca_scrape
+import bs4
+import re
 
-tmp = get_soup_of_fca_search('hsbc')
+soup = fca_scrape.get_soup_of_fca_search('Coventry Building Society')
 
+ref_number = fca_scrape.get_reference_number(soup)
