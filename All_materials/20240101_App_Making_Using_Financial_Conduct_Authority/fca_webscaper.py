6ba36f10d634c887d4cbd94c74dc0e79a7ca2928
Kenneth Quan <KEQU2@hscic.gov.uk>
2024-03-21 18:16:16 +0000

    test

--- All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
+++ All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
@@ -2,6 +2,8 @@ from src.fca_web_scrape import fca_scrape
 import bs4
 import re
 
-soup = fca_scrape.get_soup_of_fca_search('Coventry Building Society')
+soup = fca_scrape.get_soup_of_fca_search('Coventry')
 
 ref_number = fca_scrape.get_reference_number(soup)
+
+#Test
