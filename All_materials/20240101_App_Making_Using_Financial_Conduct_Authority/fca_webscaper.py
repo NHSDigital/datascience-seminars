f00e2f42ca207ffdc9ba023580966b651db90956
Kenneth Quan <KEQU2@hscic.gov.uk>
2024-03-21 22:23:34 +0000

    Getting details form each link

--- All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
+++ All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
@@ -2,8 +2,23 @@ from src.fca_web_scrape import fca_scrape
 import bs4
 import re
 
-soup = fca_scrape.get_soup_of_fca_search('Coventry')
+FIRM_REF_NUMBER = 'firm_ref_number'
+FIRM_FCA_LINK = 'fca_ref'
 
-ref_number = fca_scrape.get_reference_number(soup)
+soup = fca_scrape.get_soup_of_fca_search('Leeds Building society')
 
-#Test
+ref_table = fca_scrape.get_dict_of_ordering_firm_listings(soup)
+
+selected_link = ref_table.iloc[0][FIRM_FCA_LINK]
+
+#%%
+
+soup_data_for_firm = fca_scrape.get_soup_without_cookie_notice(selected_link)
+
+#%% 
+
+text_analysis = soup_data_for_firm.prettify()
+
+element_list = soup.find_all({'aria-hidden':'ture'})
+
+print([x.get_text() for x in element_list])
--- All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/src/fca_web_scrape/fca_scrape.py
+++ All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/src/fca_web_scrape/fca_scrape.py
@@ -5,6 +5,11 @@ import bs4
 import time
 import urllib.parse
 import re
+import pandas as pd
+
+
+FIRM_REF_NUMBER = 'firm_ref_number'
+FIRM_FCA_LINK = 'fca_ref'
 
 def get_soup_of_fca_search(search_term):
     
@@ -30,6 +35,32 @@ def get_soup_of_fca_search(search_term):
     
     return soup
 
+def get_soup_without_cookie_notice(url_link):
+    
+    driver = webdriver.Firefox()
+    
+    driver.get(url_link)
+    
+    time.sleep(10)
+    
+    element = driver.find_element(By.XPATH, '//button[@value="no"]')
+    driver.execute_script("arguments[0].click();", element)
+    
+    html_data = driver.page_source
+    
+    driver.close()
+    
+    soup = bs4.BeautifulSoup(html_data,'html.parser')
+    
+    return soup
+    
+
+def get_ref_links_from_soup(soup):
+    
+    element_list = soup.find_all('a', href=True)
+    list_of_ref = [x['href'] for x in  element_list if '/s/firm?id=' in x['href']]
+    
+    return list_of_ref
 
 def get_reference_number(soup):
     
@@ -39,3 +70,15 @@ def get_reference_number(soup):
     list_of_org_ref_number = [re.search(r'\d+', x).group() for x in  list_of_org_ref_number]
     
     return list_of_org_ref_number
+
+def get_dict_of_ordering_firm_listings(soup):
+    
+    list_of_ref_links = get_ref_links_from_soup(soup)
+    list_of_org_ref_number = get_reference_number(soup)
+    
+    dicts_of_findings = {FIRM_REF_NUMBER:list_of_org_ref_number,
+                         FIRM_FCA_LINK:list_of_ref_links}
+    
+    pandas_ref_table = pd.DataFrame(dicts_of_findings)
+    
+    return pandas_ref_table
