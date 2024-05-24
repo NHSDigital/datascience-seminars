49fa2029fc83a65a6d58f2bdebc9ecdc2949c479
Kenneth Quan <KEQU2@hscic.gov.uk>
2024-05-24 16:04:45 +0100

    Test the name

--- All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
+++ All_materials/20240101_App_Making_Using_Financial_Conduct_Authority/fca_webscaper.py
@@ -5,40 +5,11 @@ import re
 FIRM_REF_NUMBER = 'firm_ref_number'
 FIRM_FCA_LINK = 'fca_ref'
 
-soup = fca_scrape.get_soup_of_fca_search('Leeds Building society')
+soup = fca_scrape.get_soup_of_fca_search('Beehive')
 
 ref_table = fca_scrape.get_dict_of_ordering_firm_listings(soup)
 
-selected_link = ref_table.iloc[0][FIRM_FCA_LINK]
 
 #%%
 
-soup_data_for_firm = fca_scrape.get_soup_without_cookie_notice(selected_link)
-
-#%% 
-
-def get_website_from_fca_profile(soup):
-    
-    list_if_removed_extension = ['.fca.', '.gov.', '.bankofengland.']
-    
-    element_list = soup.find_all('a', {'name':'false'}, href=True)
-    
-    websites = [x['href'] for x in element_list if all(each_ext not in x['href'] for each_ext in list_if_removed_extension)]
-    
-    return websites
-
-#%%
-text_analysis = soup_data_for_firm.prettify()
-
-#element_list = soup_data_for_firm.find_all('div',{'class':'slds-col slds-size_1-of-1 slds-medium-size_6-of-12 slds-p-around_none slds-p-right_small'})
-
-#element_list = soup_data_for_firm.find_all('p', {'aria-hidden':'true'})
-
-element_list = soup_data_for_firm.find_all('a', {'name':'false'}, href=True)
-
-list_if_removed_extension = ['.fca.', '.gov.', '.bankofengland.']
-
-tmp = [x['href'] for x in element_list if all(each_ext not in x['href'] for each_ext in list_if_removed_extension)]
-
-for each_item in tmp:
-    print(each_item)
+list_of_links  = ref_table
