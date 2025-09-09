00686ec0dd4d9eb46bd9f9ad66ae1d3c950baee3
Kin Quan <kenneth.quan1@nhs.net>
2025-09-09 17:42:30 +0000

    fixed the paths

--- All_materials/20250905_Displaying_SNOMED_usage_for_ADHD_Remission_Code/config.py
+++ All_materials/20250905_Displaying_SNOMED_usage_for_ADHD_Remission_Code/config.py
@@ -1,11 +1,11 @@
 config = {
-    'file_path_to_save': 'docs/remission_calculation/source_files/',
+    'file_path_to_save': 'All_materials/20250905_Displaying_SNOMED_usage_for_ADHD_Remission_Code/source_files/',
     'SNOMED_code_usage_2023_24': 'https://files.digital.nhs.uk/70/C33DEC/SNOMED_code_usage_2023-24.xlsx',
     'SNOMED_Concept_ID':'SNOMED_Concept_ID',
     'Description':'Description',
     'Usage':'Usage',
     'code':'code',
-    'remission_code_path':'codelists/nhsd-primary-care-domain-refsets-adhdrem_cod.csv',
-    'readme_path':'docs/remission_calculation/README.md',
+    'remission_code_path':'All_materials/20250905_Displaying_SNOMED_usage_for_ADHD_Remission_Code/source_files/nhsd-primary-care-domain-refsets-adhdrem_cod.csv',
+    'readme_path':'All_materials/20250905_Displaying_SNOMED_usage_for_ADHD_Remission_Code/README.md',
     'anchor_in_readme':'[place_table_here]: #'
 }
